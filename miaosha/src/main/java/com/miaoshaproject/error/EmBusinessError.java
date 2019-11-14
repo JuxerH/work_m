9ef63495a0ce66f217934ee4716738a86e3b530d
WaterHHH <1763228073@qq.com>
2019-11-14 11:41:15 +0800

    优化前端与逻辑

--- miaosha/src/main/java/com/miaoshaproject/error/EmBusinessError.java
+++ miaosha/src/main/java/com/miaoshaproject/error/EmBusinessError.java
@@ -3,6 +3,7 @@ package com.miaoshaproject.error;
 public enum EmBusinessError implements CommonError {
     PARAMETER_VALIDATION_ERROR(10001, "参数不合法"),
     UNKNOWN_ERROR(10002, "未知错误"),
+
     USER_NOT_EXIST(20001, "用户不存在"),
     USER_LOGIN_FAIL(20002, "用户手机号或密码不正确"),
     USER_NOT_LOGIN(20003,"用户还未登录"),
